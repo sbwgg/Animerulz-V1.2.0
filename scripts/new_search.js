9ece4d4c440552ae225756505af88e425ead8dcc
jithendra7043 <144207863+jithendra7043@users.noreply.github.com>
2023-10-10 16:44:50 +0530

    Update new_search.js

--- scripts/new_search.js
+++ scripts/new_search.js
@@ -7,7 +7,6 @@ const resultsDataContainer = document.getElementsByClassName("anilist-search-dat
 const searchIcon = document.getElementsByClassName("search-box-icon__")[0];
 let key = 0;
 
-
 if(searchBox.value)
     anilistContainer.style.display = "flex";
 else
@@ -192,51 +191,61 @@ function handleResponse(response) {
 }
 
 function handleData(data) {
+    console.log(data);
     resultsDataContainer.innerHTML = "";
     let animeCount = 4;
     let requiredAnimeData = data.data.Page.media;
-    let temp;
-    for(let i = 0; i < animeCount; i ++){
-        temp = "";
-        if(requiredAnimeData[i].title.english != null){
-        try{
-            temp = `<li class="item-search-data_" id="element-${i}" onclick="window.open('https://animerulz.in/${requiredAnimeData[i].title.english.replace(/[^a-zA-Z0-9]/g, "").toLowerCase()}', '_self')">
-            <div class="image-data"><img src="${requiredAnimeData[i].coverImage.medium}" alt="" class="img-search-res"></div>
-            <div class="search-anime-data-res">
-            <div class="head-data-search-res">
-                    <span class="anime-name-search-res">${requiredAnimeData[i].title.english}</span>
-            </div>
-            <div class="bottom-data-search-res">
-                <div class="btm-div-1">
-                    <div class="anime-info-search-data">
-                        <div class="anime-info-item-search">Episodes : ${requiredAnimeData[i].episodes}</div>
-                        <div class="anime-info-item-search">${requiredAnimeData[i].format}</div>
-                        <div class="anime-info-item-search">Score : ${requiredAnimeData[i].averageScore}</div>
-                        </div>
-                </div>
-                <div class="btm-div-2">
-                    <div class="anime-info-search-data__">
-                        <div class="anime-info-item-search__">Start : ${requiredAnimeData[i].startDate.year}</div>
-                        <div class="anime-info-item-search__">End : ${requiredAnimeData[i].endDate.year}</div>
-                        </div>
-                </div>
-            </div>
-            </div>
-        </li>`;
-        }
-        catch{
-            // i --;
-        }
+    console.log(requiredAnimeData);
+    if(requiredAnimeData.length == 0){
+        resultsDataContainer.innerHTML = ` <div class="no-results-found-section">
+        <span>No Results Found</span>
+     </div>`;
+     document.getElementsByClassName("more-results-sec")[0].style.display = "none";
     }
-    let listItem = document.createElement("li");
-    listItem.innerHTML = temp;
-     resultsDataContainer.appendChild(listItem);
+    else{
+        let temp;
+        for(let i = 0; i < animeCount; i ++){
+            temp = "";
+            if(requiredAnimeData[i].title.english != null){
+            try{
+                temp = `<li class="item-search-data_" id="element-${i}" onclick="window.open('https://animerulz.in/${requiredAnimeData[i].title.english.replace(/[^a-zA-Z0-9]/g, "").toLowerCase()}', '_self')">
+                <div class="image-data"><img src="${requiredAnimeData[i].coverImage.medium}" alt="" class="img-search-res"></div>
+                <div class="search-anime-data-res">
+                <div class="head-data-search-res">
+                        <span class="anime-name-search-res">${requiredAnimeData[i].title.english}</span>
+                </div>
+                <div class="bottom-data-search-res">
+                    <div class="btm-div-1">
+                        <div class="anime-info-search-data">
+                            <div class="anime-info-item-search">Episodes : ${requiredAnimeData[i].episodes}</div>
+                            <div class="anime-info-item-search">${requiredAnimeData[i].format}</div>
+                            <div class="anime-info-item-search">Score : ${requiredAnimeData[i].averageScore}</div>
+                            </div>
+                    </div>
+                    <div class="btm-div-2">
+                        <div class="anime-info-search-data__">
+                            <div class="anime-info-item-search__">Start : ${requiredAnimeData[i].startDate.year}</div>
+                            <div class="anime-info-item-search__">End : ${requiredAnimeData[i].endDate.year}</div>
+                            </div>
+                    </div>
+                </div>
+                </div>
+            </li>`;
+            }
+            catch{
+                // i --;
+            }
+        }
+        let listItem = document.createElement("li");
+        listItem.innerHTML = temp;
+        resultsDataContainer.appendChild(listItem);
+        }
+        let newTemp = `<div class="data-more-results" onclick='window.open("/search/?anime=${searchBox.value}", "_self")'>
+        <span class="text-more-results">More Results</span>
+        <svg xmlns="http://www.w3.org/2000/svg" height="1em" class="svg-more-res" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M52.5 440.6c-9.5 7.9-22.8 9.7-34.1 4.4S0 428.4 0 416V96C0 83.6 7.2 72.3 18.4 67s24.5-3.6 34.1 4.4L224 214.3V256v41.7L52.5 440.6zM256 352V256 128 96c0-12.4 7.2-23.7 18.4-29s24.5-3.6 34.1 4.4l192 160c7.3 6.1 11.5 15.1 11.5 24.6s-4.2 18.5-11.5 24.6l-192 160c-9.5 7.9-22.8 9.7-34.1 4.4s-18.4-16.6-18.4-29V352z"/></svg>
+    </div>`;
+        document.getElementsByClassName("more-results-sec")[0].innerHTML = newTemp;
     }
-    let newTemp = `<div class="data-more-results" onclick='window.open("/search/?anime=${searchBox.value}", "_self")'>
-    <span class="text-more-results">More Results</span>
-    <svg xmlns="http://www.w3.org/2000/svg" height="1em" class="svg-more-res" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M52.5 440.6c-9.5 7.9-22.8 9.7-34.1 4.4S0 428.4 0 416V96C0 83.6 7.2 72.3 18.4 67s24.5-3.6 34.1 4.4L224 214.3V256v41.7L52.5 440.6zM256 352V256 128 96c0-12.4 7.2-23.7 18.4-29s24.5-3.6 34.1 4.4l192 160c7.3 6.1 11.5 15.1 11.5 24.6s-4.2 18.5-11.5 24.6l-192 160c-9.5 7.9-22.8 9.7-34.1 4.4s-18.4-16.6-18.4-29V352z"/></svg>
-   </div>`;
-    document.getElementsByClassName("more-results-sec")[0].innerHTML = newTemp;
     // console.log(data);
     
     // console.log(data.data.Media.relations.edges[0].node);
