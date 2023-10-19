4c7ecb8ec7ec4673e39c618304bea6fc4240407d
jithendra7043 <144207863+jithendra7043@users.noreply.github.com>
2023-10-19 20:56:13 +0530

    Update long-anime.js

--- scripts/long-anime.js
+++ scripts/long-anime.js
@@ -1,6 +1,5 @@
 const animeNameee = document.getElementById("a-active-page-tag").textContent.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, "").trim();
 
-
 const longAnimes = ['one piece',
 'naruto',
 'naruto shippuden',
