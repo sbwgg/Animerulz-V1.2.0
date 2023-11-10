32f77f7d251856d9c06059f9773fad8010881147
jithendra7043 <jithendrakavala@gmail.com>
2023-11-10 09:08:12 +0530

    Update redirectFromMainPage.js

--- scripts/redirectFromMainPage.js
+++ scripts/redirectFromMainPage.js
@@ -2,44 +2,43 @@ const url = window.location.href;
 // url = "https://animerulz.in/Naruto/"
 const urlList = url.split("/");
 let index;
+console.log(urlList.length);
 
-const redirectThis = [
-    "myheroacademiaseason2",
-    "Naruto",
-    "MyHeroAcademia",
-    "DragonBallSuper",
-    "jujutsukaisenseason2",
-    "tokyorevengersseason2",
-    "rurounikenshin2023",
-    "attackontitanfinalseason",
-    "bleachthousandyearbloodwar",
-    "tokyorevengersseason2part2",
-    "attackontitanfinalseasonthefinalchaptersspecial1",
-    "attackontitanfinalseasonthefinalchaptersspecial2",
-    "attackontitanfinalseasonpart3"
-]
-const redirectTo = [
-    "myheroacademia2",
-    "naruto",
-    "myheroacademia",
-    "dragonballsuper",
-    "jujutsukaisen2ndseason",
-    "tokyorevengerschristmasshowdown",
-    "rurounikenshinmeijikenkakuromantan2023",
-    "attackontitanfinalseasonpart1",
-    "bleachthousandyearbloodwararc",
-    "tokyorevengerstenjikuhen",
-    "attackontitanthefinalseasonpart3",
-    "attackontitanthefinalseasonpart3",
-    "attackontitanthefinalseasonpart3"
-]
+if(urlList.length > 4){
+    window.location.href = "https://animerulz.in/"
+}else{
+    const redirectThis = [
+        "myheroacademiaseason2",
+        "Naruto",
+        "MyHeroAcademia",
+        "DragonBallSuper",
+        "jujutsukaisenseason2",
+        "tokyorevengersseason2",
+        "rurounikenshin2023",
+        "attackontitanfinalseason",
+        "bleachthousandyearbloodwar",
+        "tokyorevengersseason2part2"
+    ]
+    const redirectTo = [
+        "myheroacademia2",
+        "naruto",
+        "myheroacademia",
+        "dragonballsuper",
+        "jujutsukaisen2ndseason",
+        "tokyorevengerschristmasshowdown",
+        "rurounikenshinmeijikenkakuromantan2023",
+        "attackontitanfinalseasonpart1",
+        "bleachthousandyearbloodwararc",
+        "tokyorevengerstenjikuhen"
+    ]
 
-for(let i = 1; i <= redirectThis.length ; i ++){
-    if(urlList[3] == redirectThis[i - 1]){
-        index = i;
-        break;
+    for(let i = 1; i <= redirectThis.length ; i ++){
+        if(urlList[3] == redirectThis[i - 1]){
+            index = i;
+            break;
+        }
     }
+    // console.log(index);
+    if(index)
+        window.location.href = "https://animerulz.in/" + redirectTo[index - 1];
 }
-// console.log(index);
-if(index)
-    window.location.href = "https://animerulz.in/" + redirectTo[index - 1];
