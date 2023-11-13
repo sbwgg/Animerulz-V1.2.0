c9196eb7f200db87e3a65dbb8b3b9813950084bd
jithendra7043 <jithendrakavala@gmail.com>
2023-11-13 13:29:58 +0530

    Update long-anime.js

--- scripts/long-anime.js
+++ scripts/long-anime.js
@@ -445,12 +445,12 @@ let anime = document.getElementById("a-active-page-tag");
             }
             catch{}
             if(i == epi_num){
-                temp = `<div class='episode active ${fillerFlag}' title="${current_short_anime_data['episodes_names'][i - 1]}" id='` + anime + "-episode-" + i + "'>" + i + "</div>";
+                temp = `<div class='episode active ${fillerFlag}' title="${episodeNameNew}" id='` + anime + "-episode-" + i + "'>" + i + "</div>";
                 if(fillerFlag === 'filler-episode')
                     document.querySelector("span.filler-alert").innerHTML = '( <i class="fa-solid fa-skull-crossbones fa-fade"></i> filler episode)'
             }else{ 
                 var temp_1 = 'window.open("https://animerulz.in/' + anime.toLowerCase() + "/Watch-Now/?ep=" + i + '&aud=' + epi_aud + '", "_self")';
-                temp = `<div class='episode ${fillerFlag}' title="${current_short_anime_data['episodes_names'][i - 1]}" id='` + anime + "-episode-" + i + "' onclick='" + temp_1 + "'>" + i +"</div>" ;
+                temp = `<div class='episode ${fillerFlag}' title="${episodeNameNew}" id='` + anime + "-episode-" + i + "' onclick='" + temp_1 + "'>" + i +"</div>" ;
                 // temp = "<div class='episode' id='episode-" + i + "'>" + i +"</div>" ;
             }
             outputNew += temp;
@@ -1121,7 +1121,7 @@ function setShortAnimes(short_anime_data){
                 }
                 catch{}
                 if(i == epi_num){
-                    temp = ` <div class="episode-short ${fillerFlag}" title="${current_short_anime_data['episodes_names'][i - 1]}"><div class="short-anime-ep active-episode" id="` + anime + '-episode-' + i + '"> ' + 
+                    temp = ` <div class="episode-short ${fillerFlag}" title="${episodeNameNew}"><div class="short-anime-ep active-episode" id="` + anime + '-episode-' + i + '"> ' + 
                     '<div class="short-anime-num episode-alternate-' + first + '">' + i + '</div><div class="short-anime-name episode-alternate-' + second + '"><span>' + episodeNameNew + '</span> ' + 
                     '<svg xmlns="http://www.w3.org/2000/svg" class="play-svg" enable-background="new 0 0 100 100" viewBox="0 0 100 100"><switch><g><path d="M5273.1,2400.1v-2c0-2.8-5-4-9.7-4s-9.7,1.3-9.7,4v2c0,1.8,0.7,3.6,2,4.9l5,4.9c0.3,0.3,0.4,0.6,0.4,1v6.4c0,0.4,0.2,0.7,0.6,0.8l2.9,0.9c0.5,0.1,1-0.2,1-0.8v-7.2c0-0.4,0.2-0.7,0.4-1l5.1-5C5272.4,2403.7,5273.1,2401.9,5273.1,2400.1zM5263.4,2400c-4.8,0-7.4-1.3-7.5-1.8v0c0.1-0.5,2.7-1.8,7.5-1.8c4.8,0,7.3,1.3,7.5,1.8C5270.7,2398.7,5268.2,2400,5263.4,2400z"/><path d="M5268.4 2410.3c-.6 0-1 .4-1 1 0 .6.4 1 1 1h4.3c.6 0 1-.4 1-1 0-.6-.4-1-1-1H5268.4zM5272.7 2413.7h-4.3c-.6 0-1 .4-1 1 0 .6.4 1 1 1h4.3c.6 0 1-.4 1-1C5273.7 2414.1 5273.3 2413.7 5272.7 2413.7zM5272.7 2417h-4.3c-.6 0-1 .4-1 1 0 .6.4 1 1 1h4.3c.6 0 1-.4 1-1C5273.7 2417.5 5273.3 2417 5272.7 2417zM50 2.5C23.8 2.5 2.5 23.8 2.5 50c0 26.2 21.3 47.5 47.5 47.5 26.2 0 47.5-21.3 47.5-47.5C97.5 23.8 76.2 2.5 50 2.5zM67.2 52.8L41.6 69.8c-2.2 1.5-5.1-.1-5.1-2.8V32.9c0-2.6 2.9-4.2 5.1-2.8l25.6 17.1C69.2 48.6 69.2 51.4 67.2 52.8z"/></g></switch></svg> ' + 
                     '</div></div></div>';
@@ -1130,7 +1130,7 @@ function setShortAnimes(short_anime_data){
                 }
                 else{
                     temp_1 = "window.open('https://animerulz.in/" + anime.toLowerCase() + "/Watch-Now/?ep=" + i + "&aud=" + epi_aud + "', '_self')";
-                    temp = `<div class="episode-short ${fillerFlag}" title="${current_short_anime_data['episodes_names'][i - 1]}" onclick="` + temp_1 + '"><div class="short-anime-ep"  id="' + anime + '-episode-' + i + '"><div class="short-anime-num episode-alternate-' + first + '">' + i +'</div><div class="short-anime-name episode-alternate-' + second + '"><span>' + episodeNameNew + '</span></div></div></div>'
+                    temp = `<div class="episode-short ${fillerFlag}" title="${episodeNameNew}" onclick="` + temp_1 + '"><div class="short-anime-ep"  id="' + anime + '-episode-' + i + '"><div class="short-anime-num episode-alternate-' + first + '">' + i +'</div><div class="short-anime-name episode-alternate-' + second + '"><span>' + episodeNameNew + '</span></div></div></div>'
                 }
                 outputNew += temp;
             }
